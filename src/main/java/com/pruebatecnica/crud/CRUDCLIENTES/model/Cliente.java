795802d2b303e99cdf1f6ada82344d434d853f2d
GILZX <gilrz0101@gmail.com>
2024-09-07 16:15:41 -0600

    fix:validaciones De Campos y Entrega Segunda Etapa

--- src/main/java/com/pruebatecnica/crud/CRUDCLIENTES/model/Cliente.java
+++ src/main/java/com/pruebatecnica/crud/CRUDCLIENTES/model/Cliente.java
@@ -6,7 +6,9 @@ import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonEncoding;
@@ -15,148 +17,135 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
-@Table(name="clientes")
+@Table(name = "clientes")
 public class Cliente {
 
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-private int id_cliente;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_cliente;
 
-@NotBlank(message="Es requerido el valor del nombre")
-private String nombre_cliente;
+	@NotBlank(message = "Es requerido el valor del nombre")
+	private String nombre_cliente;
 
-@NotBlank(message="Es requerido el valor de apellido")
-private String apellido_cliente;
+	@NotBlank(message = "Es requerido el valor de apellido")
+	private String apellido_cliente;
 
+	private String correo_electronico;
 
-private String correo_electronico;
+	@NotNull(message = "El campo edad es requerido")
+	@Min(value = 1, message = "Verifique el valor minimo de la edad")
+	@Max(value = 105, message = "Verigfique el valor maximo de la edad")
+	private Integer edad;
 
-@Min(value=1,message="Verifique el valor minimo de la edad")
-@Max(value=105,message="Verigfique el valor maximo de la edad")
-private int edad;
+	@NotBlank(message = "Es requerido el genero")
+	@Pattern(regexp = "M|F", message = "Valores Requeridos M masculino o F para Femenino")
+	private String genero;
 
-@NotBlank(message="Es requerido el genero")
-@Pattern(regexp = "M|F",message="Valores Requeridos M masculino o F para Femenino")
-private String genero;
+	private String direccion;
 
-private String direccion;
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@CreationTimestamp
+	@Column(updatable = false)
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private Date fecha_registro;
 
-@JsonProperty(access=JsonProperty.Access.READ_ONLY)
-@CreationTimestamp
-@Column(updatable = false)
-private Date fecha_registro;
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+	private OffsetDateTime ultima_modificacion;
 
-@JsonProperty(access=JsonProperty.Access.READ_ONLY)
-@UpdateTimestamp
-private OffsetDateTime  ultima_modificacion;
+	public Cliente() {
 
+	}
 
-public Cliente() {
-	
-}
+	public Cliente(String nombre_cliente, String apellido_cliente, String correo_electronico, Integer edad,
+			String genero, String direccion, Date fecha_registro, OffsetDateTime ultima_modificacion) {
+		this.nombre_cliente = nombre_cliente;
+		this.apellido_cliente = apellido_cliente;
+		this.correo_electronico = correo_electronico;
+		this.edad = edad;
+		this.genero = genero;
+		this.direccion = direccion;
+		this.fecha_registro = fecha_registro;
+		this.ultima_modificacion = ultima_modificacion;
+	}
 
+	public String getNombre_cliente() {
+		return nombre_cliente;
+	}
 
+	public void setNombre_cliente(String nombre_cliente) {
+		this.nombre_cliente = nombre_cliente;
+	}
 
+	public String getApellido_cliente() {
+		return apellido_cliente;
+	}
 
-public Cliente(String nombre_cliente, String apellido_cliente, String correo_electronico, int edad,
-		String genero, String direccion, Date fecha_registro, OffsetDateTime ultima_modificacion) {
-	this.nombre_cliente = nombre_cliente;
-	this.apellido_cliente = apellido_cliente;
-	this.correo_electronico = correo_electronico;
-	this.edad = edad;
-	this.genero = genero;
-	this.direccion = direccion;
-	this.fecha_registro = fecha_registro;
-	this.ultima_modificacion = ultima_modificacion;
-}
+	public int getId_cliente() {
+		return id_cliente;
+	}
 
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
 
+	public void setApellido_cliente(String apellido_cliente) {
+		this.apellido_cliente = apellido_cliente;
+	}
 
-public String getNombre_cliente() {
-	return nombre_cliente;
-}
+	public String getCorreo_electronico() {
+		return correo_electronico;
+	}
 
-public void setNombre_cliente(String nombre_cliente) {
-	this.nombre_cliente = nombre_cliente;
-}
+	public void setCorreo_electronico(String correo_electronico) {
+		this.correo_electronico = correo_electronico;
+	}
 
-public String getApellido_cliente() {
-	return apellido_cliente;
-}
+	public int getEdad() {
+		return edad;
+	}
 
-public int getId_cliente() {
-	return id_cliente;
-}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
 
+	public String getGenero() {
+		return genero;
+	}
 
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 
+	public String getDireccion() {
+		return direccion;
+	}
 
-public void setId_cliente(int id_cliente) {
-	this.id_cliente = id_cliente;
-}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 
+	public Date getFecha_registro() {
+		return fecha_registro;
+	}
 
+	public void setFecha_registro(Date fecha_registro) {
+		this.fecha_registro = fecha_registro;
+	}
 
+	public OffsetDateTime getUltima_modificacion() {
+		return ultima_modificacion;
+	}
 
-public void setApellido_cliente(String apellido_cliente) {
-	this.apellido_cliente = apellido_cliente;
-}
+	public void setUltima_modificacion(OffsetDateTime ultima_modificacion) {
+		this.ultima_modificacion = ultima_modificacion;
+	}
 
-public String getCorreo_electronico() {
-	return correo_electronico;
-}
-
-public void setCorreo_electronico(String correo_electronico) {
-	this.correo_electronico = correo_electronico;
-}
-
-public int getEdad() {
-	return edad;
-}
-
-public void setEdad(int edad) {
-	this.edad = edad;
-}
-
-public String getGenero() {
-	return genero;
-}
-
-public void setGenero(String genero) {
-	this.genero = genero;
-}
-
-public String getDireccion() {
-	return direccion;
-}
-
-public void setDireccion(String direccion) {
-	this.direccion = direccion;
-}
-
-public Date getFecha_registro() {
-	return fecha_registro;
-}
-
-public void setFecha_registro(Date fecha_registro) {
-	this.fecha_registro = fecha_registro;
-}
-
-public OffsetDateTime getUltima_modificacion() {
-	return ultima_modificacion;
-}
-
-public void setUltima_modificacion(OffsetDateTime ultima_modificacion) {
-	this.ultima_modificacion = ultima_modificacion;
-}
-
-
-
-
-	
-	
-	
 }
--- src/main/java/com/pruebatecnica/crud/CRUDCLIENTES/rest/ClienteRestController.java
+++ src/main/java/com/pruebatecnica/crud/CRUDCLIENTES/rest/ClienteRestController.java
@@ -23,140 +23,126 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteRestController {
-	
-	private ClienteJpaRepository clienteRepository ;
-	
+
+	private ClienteJpaRepository clienteRepository;
+
 	@Autowired
 	public ClienteRestController(ClienteJpaRepository cliente) {
-		this.clienteRepository=cliente;
+		this.clienteRepository = cliente;
 	}
-	
+
 	@GetMapping
-	 public List<Cliente> getAllClientes() {
-		
-	        return clienteRepository.findAll();
-	        
-	        
-	    }
-	
-	
+	public List<Cliente> getAllClientes() {
+
+		return clienteRepository.findAll();
+
+	}
+
 	@PostMapping("/buscar/{id}")
-	 public ResponseEntity<MessageResponse> buscarCliente(@PathVariable Integer id) {
-		
-			 Optional <Cliente> cliente=clienteRepository.findById(id);
-			
-					 
-				
-					 
-					 if(cliente.isPresent()) {
-						 MessageResponse message=new MessageResponse();
-						 message.setStatus(HttpStatus.OK);
-						 message.setErrores(null);
-						 message.setResponse(cliente.get());
-						 message.setMensaje("Cliente Encontrado Con Exito");
-						 return new ResponseEntity<>(message,HttpStatus.OK);
-						 
-					 }else {
-						 MessageResponse message=new MessageResponse();
-						 message.setStatus(HttpStatus.NOT_FOUND);
-						 message.setResponse(null);
-						 message.setMensaje("No Se encontro el cliente solicitado");
-						 message.setErrores(HttpStatus.NOT_FOUND.getReasonPhrase());
-							
-						return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
-					 }
-						 
-					
-				
-			
-					
-			
-			 
-			}
-			
-		
-	    
-	
+	public ResponseEntity<MessageResponse> buscarCliente(@PathVariable Integer id) {
+
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if (cliente.isPresent()) {
+			MessageResponse message = new MessageResponse();
+			message.setStatus(HttpStatus.OK);
+			message.setErrores(null);
+			message.setResponse(cliente.get());
+			message.setMensaje("Cliente Encontrado Con Exito");
+			return new ResponseEntity<>(message, HttpStatus.OK);
+
+		} else {
+			MessageResponse message = new MessageResponse();
+			message.setStatus(HttpStatus.NOT_FOUND);
+			message.setResponse(null);
+			message.setMensaje("No Se encontro el cliente solicitado");
+			message.setErrores(HttpStatus.NOT_FOUND.getReasonPhrase());
+
+			return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+		}
+
+	}
+
 	@PostMapping("/nuevoCliente")
 	public ResponseEntity<MessageResponse> guardarCliente(@Valid @RequestBody Cliente cliente) {
+
+		MessageResponse message = new MessageResponse();
 		
 		try {
-			MessageResponse message=new MessageResponse();
-		
-			Cliente saveCliente=clienteRepository.save(cliente);
-			
+			Cliente saveCliente = clienteRepository.save(cliente);
 			message.setResponse(saveCliente);
 			message.setErrores(null);
 			message.setMensaje(" Nuevo Cliente Creado Con Exito");
 			message.setStatus(HttpStatus.OK);
+
+			return new ResponseEntity<>(message, HttpStatus.OK);
+
+		} catch (Exception e) {
 			
-			return new ResponseEntity<>(message,HttpStatus.OK);
-		
-			
-		}catch(Exception e) {
-			MessageResponse message=new MessageResponse();
 			message.setResponse(null);
 			message.setErrores(e.getMessage());
 			message.setMensaje("Error al Guardar Los Datos del Cliente");
 			message.setStatus(HttpStatus.BAD_REQUEST);
-			
-			return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+
+			return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 		}
-		
-	}	
-		
-		
-	
-	
+
+	}
+
 	@PutMapping("/actualizarCliente/{id}")
-	public ResponseEntity<MessageResponse> actualizarCliente(@PathVariable Integer id,@Valid @RequestBody Cliente cliente,BindingResult result) {
-		if(clienteRepository.findById(id).isPresent()) {
-			cliente.setId_cliente(id);
-			cliente.setFecha_registro(clienteRepository.findById(id).get().getFecha_registro());
-			Cliente saveCliente=clienteRepository.save(cliente);
-			
-			MessageResponse message=new MessageResponse();
-			message.setResponse(saveCliente);
+	public ResponseEntity<MessageResponse> actualizarCliente(@PathVariable Integer id,
+			@Valid @RequestBody Cliente data, BindingResult result) {
+		MessageResponse message = new MessageResponse();
+		Optional<Cliente> entityCliente=clienteRepository.findById(id);
+	
+		
+		
+		if (entityCliente.isPresent()) {
+			Cliente cliente=entityCliente.get();
+			cliente.setNombre_cliente(data.getNombre_cliente());
+			cliente.setApellido_cliente(data.getApellido_cliente());
+			cliente.setEdad(data.getEdad());
+			cliente.setCorreo_electronico(data.getCorreo_electronico());
+			cliente.setGenero(data.getGenero());
+			clienteRepository.save(cliente);
+			message.setResponse(cliente);
 			message.setErrores(result.getFieldErrors().toString());
 			message.setMensaje("Datos del Cliente Actualizados Correctamente");
 			message.setStatus(HttpStatus.OK);
-			
-		 return new ResponseEntity<>(message,HttpStatus.OK);
-		}else {
-			MessageResponse message=new MessageResponse();
+
+			return new ResponseEntity<>(message, HttpStatus.OK);
+		} else {
 			message.setResponse(null);
 			message.setErrores(result.getFieldErrors().toString());
 			message.setMensaje("No Fue posible actualizar el cliente".concat("no existe el cliente con id " + id));
 			message.setStatus(HttpStatus.NOT_FOUND);
-			 return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
 		}
-		
-		
+
 	}
-	
-	
+
 	@PostMapping("/eliminarCliente/{id}")
-	public ResponseEntity<MessageResponse> eliminarCliente(@PathVariable Integer id){
-		
-		
-		MessageResponse message=new MessageResponse();
-		
-		//Comentario Añadido
-		if(clienteRepository.findById(id).isPresent()) {
+	public ResponseEntity<MessageResponse> eliminarCliente(@PathVariable Integer id) {
+
+		MessageResponse message = new MessageResponse();
+
+		// Comentario Añadido
+		if (clienteRepository.findById(id).isPresent()) {
 			message.setErrores(null);
 			message.setMensaje("El Cliente Fue Eliminado Con Exito");
 			message.setResponse(null);
 			message.setStatus(HttpStatus.OK);
 			clienteRepository.deleteById(id);
-			return new ResponseEntity(message,HttpStatus.OK);
-			
-		}else {
-			return new ResponseEntity(message,HttpStatus.NOT_FOUND);
+			return new ResponseEntity(message, HttpStatus.OK);
+
+		} else {
+			message.setErrores(null);
+			message.setMensaje("No fue posible encontrar el cliente");
+			message.setResponse(null);
+			message.setStatus(HttpStatus.NOT_FOUND);
+			return new ResponseEntity(message, HttpStatus.NOT_FOUND);
 
 		}
-		
-		
+
 	}
-	
-	
+
 }
